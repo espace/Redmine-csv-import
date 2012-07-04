1296a6ada4e84d403b77ccea3d4faecd73cb6a64
Alfred <alfred.nagy@espace.com.eg>
2012-07-04 13:50:59 +0200

    FIX: add partial full path in hooks to make it work from different controllers

--- lib/import_from_csv_hooks.rb
+++ lib/import_from_csv_hooks.rb
@@ -1,3 +1,3 @@
 class ImportFromCsvHooks < Redmine::Hook::ViewListener
-  render_on :view_issues_sidebar_issues_bottom, :partial => 'import_issues_from_csv'
+  render_on :view_issues_sidebar_issues_bottom, :partial => 'issues/import_issues_from_csv'
 end
